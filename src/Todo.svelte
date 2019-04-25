5bd13a453461295e4040065d56bebed7cede7869
Michael Savoia <msavoiadev@gmail.com>
2019-04-24 21:50:36 -0400

    Added todo search, base and key list off of todo id, and store todos in local storage

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,39 +1,53 @@
 <script>
-  export let props;
-  let { todo, i, toggleTodoCompleted, deleteTodo } = props;
+  export let props
+  let { todo, toggleTodoCompleted, deleteTodo } = props
 </script>
 
 <style>
-  .todo-item {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 8px 0;
-  }
+    .todo-item {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        padding: 8px 0;
+    }
 
-  .todo-item p {
-    font-size: 1.1rem;
-  }
+    .todo-item p {
+        font-size: 1.1rem;
+    }
 
-  .todo-item.completed p {
-    text-decoration: line-through;
-  }
+    .todo-item.completed p {
+        text-decoration: line-through;
+        color: #b1aeb5;
+    }
 
-  .todo-item-group {
-    display: flex;
-    align-items: center;
-  }
+    .todo-item-group {
+        display: flex;
+        align-items: center;
+    }
 
-  /* TODO: Create custom checkbox */
-  .todo-toggle {
-    opacity: 0;
-  }
+    /* TODO: Create custom checkbox */
+    .todo-toggle {
+        opacity: 0;
+    }
+
+    .todo-delete {
+        color: #6b1f33;
+        background: none;
+        border: none;
+        box-shadow: none;
+    }
+
+    .todo-delete:hover {
+        background: #ea6b8d;
+    }
 </style>
 
 <li class="todo-item"  class:completed={todo.completed}>
   <div class="todo-item-group">
-    <input type="checkbox" class="todo-toggle" on:click={toggleTodoCompleted} bind:checked={todo.completed} value={i} />
+    <input type="checkbox" class="todo-toggle" on:click={toggleTodoCompleted} bind:checked={todo.completed} value={todo.id} />
     <p>{todo.title}</p>
   </div>
-  <button class="todo-delete btn btn-soft danger" on:click={deleteTodo} value={i}>Delete Todo</button>
+  <button class="todo-delete btn" on:click={deleteTodo} value={todo.id}>
+    Delete
+  </button>
 </li>
--- src/Todos.svelte
+++ src/Todos.svelte
@@ -1,138 +1,194 @@
 <script>
-  import { tick } from "svelte";
-  import Todo from "./Todo.svelte";
-  let todos = [];
-  let newTodo = "";
+    import { tick } from 'svelte'
+    import Todo from './Todo.svelte'
+    let todos = JSON.parse(localStorage.getItem('todos')) || []
+    let newTodo = ''
+    let filterTerm = ''
+    $: filteredTodos = todos.filter(todo =>
+        filterTerm ? todo.title.includes(filterTerm) : true
+    )
 
-  const addNewTodo = async () => {
-    if (newTodo !== "") {
-      todos = [
-        ...todos,
-        {
-          title: newTodo,
-          completed: false
+    const addNewTodo = async () => {
+        if (newTodo !== '') {
+            todos = [
+                ...todos,
+                {
+                    title: newTodo,
+                    completed: false,
+                    id: Date.now(),
+                },
+            ]
+            localStorage.setItem('todos', JSON.stringify(todos))
+            newTodo = ''
+            await tick()
+            const todosList = document.getElementById('todos-list-group')
+            todosList.scrollTop = todosList.scrollHeight
         }
-      ];
-      newTodo = "";
-      await tick();
-      const todosList = document.getElementById("todos-list");
-      todosList.scrollTop = todosList.scrollHeight;
     }
-  };
 
-  const toggleTodoCompleted = e => {
-    todos = todos.map((todo, index) => {
-      if (e.target.value == index) {
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-      } else {
-        return todo;
-      }
-    });
-  };
+    const toggleTodoCompleted = e => {
+        todos = todos.map(todo => {
+            if (e.target.value == todo.id) {
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                }
+            } else {
+                return todo
+            }
+        })
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }
 
-  const deleteTodo = e => {
-    todos = todos.filter((todo, index) => e.target.value != index);
-  };
+    const deleteTodo = e => {
+        todos = todos.filter(todo => e.target.value != todo.id)
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }
 </script>
 
 <style>
-  .wrapper {
-    margin: 40px auto;
-    background: #fff;
-    max-width: 800px;
-    height: 100%;
-    max-height: calc(100vh - 80px);
-    width: 95%;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-  }
-
-  .todos-info {
-    padding: 10px 15px;
-    border-bottom: 1px solid #404041;
-    display: flex;
-    align-items: baseline;
-    flex-wrap: wrap;
-  }
-
-  .todos-info .badge {
-    margin: 10px 0;
-    margin-right: 20px;
-  }
-
-  .todos-info .badge:last-child {
-    margin-right: 0;
-  }
-
-  .todos-list {
-    margin: 0;
-    padding: 20px 15px;
-    list-style-type: none;
-    overflow: auto;
-  }
-
-  .total-todos-count {
-    background: #fff;
-    color: #404041;
-  }
-
-  .todos-left-count {
-    background: #ea6b8d;
-    color: #6b1f33;
-  }
-
-  .completed-todos-count {
-    background: #91d595;
-    color: hsl(124, 65%, 30%);
-  }
-
-  .add-todo-group {
-    padding: 20px 15px;
-    border-top: 1px solid #404041;
-  }
-
-  .add-todo-group form {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .add-todo-group form input {
-    flex-grow: 1;
-    margin-right: 20px;
-  }
-
-  @media (min-width: 576px) {
     .wrapper {
-      width: 90%;
+        margin: 40px auto;
+        background: #fff;
+        max-width: 800px;
+        height: calc(100vh - 80px);
+        width: 95%;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        overflow: hidden;
     }
-  }
+
+    .todos-info {
+        display: flex;
+        padding: 10px 15px;
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+        border-bottom: 2px solid #5d58ad;
+    }
+
+    .todos-info input {
+        margin: 5px 0;
+    }
+
+    .badge-group {
+        flex-grow: 1;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .badge-group .badge {
+        margin: 10px 0;
+        margin-right: 20px;
+    }
+
+    .todos-list-group {
+        overflow: auto;
+    }
+
+    .todos-list {
+        margin: 0;
+        padding: 8px 15px;
+        list-style-type: none;
+    }
+
+    .empty-todos-list {
+        margin: 0;
+        padding: 20px 15px;
+        font-size: 1.2rem;
+        color: #b1aeb5;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .total-todos-count {
+        background: #fff;
+        color: #404041;
+    }
+
+    .todos-left-count {
+        background: #ea6b8d;
+        color: #6b1f33;
+    }
+
+    .completed-todos-count {
+        background: #91d595;
+        color: hsl(124, 65%, 30%);
+    }
+
+    .add-todo-group {
+        padding: 20px 15px;
+        border-top: 2px solid #5d58ad;
+    }
+
+    .add-todo-group form {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .add-todo-group form input {
+        flex-grow: 1;
+        margin-right: 20px;
+        box-shadow: inset 0 2px 4px 0 rgba(0, 0, 0, 0.06);
+    }
+
+    .btn {
+        box-shadow: 0 4px 6px rgba(50, 50, 50, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+        transition: all 0.15s ease;
+        background: #5d58ad;
+        color: #fff;
+        border: none;
+    }
+
+    .btn:hover {
+        box-shadow: 0 7px 12px rgba(50, 50, 50, 0.1), 0 2px 4px rgba(0, 0, 0, 0.08);
+        background: #7670c7;
+        transform: translateY(-1px);
+    }
+
+    .btn:active {
+        background: #4a458c;
+        transform: translateY(1px);
+    }
+
+    @media (min-width: 576px) {
+        .wrapper {
+            width: 90%;
+        }
+    }
 </style>
 
 <div class="wrapper">
   <div class="todos-info">
+  <div class="badge-group">
     <div class="badge total-todos-count">{todos.length} total todos</div>
-    {#if todos.length}
-      <div class="badge completed-todos-count">{todos.filter(todo => todo.completed).length} completed todos</div>
-      <div class="badge todos-left-count">{todos.filter(todo => !todo.completed).length} left</div>
+      {#if todos.length}
+        <div class="badge completed-todos-count">{todos.filter(todo => todo.completed).length} completed todos</div>
+        <div class="badge todos-left-count">{todos.filter(todo => !todo.completed).length} left</div>
+      {/if}
+    </div>
+    <div>
+      <input type="text" bind:value={filterTerm} placeholder="Filter todos...">
+    </div>
+  </div>
+  <div class="todos-list-group" id="todos-list-group">
+    {#if filteredTodos.length}
+      <ul class="todos-list">
+        {#each filteredTodos as todo (todo.id)}
+          <Todo props={{todo, deleteTodo, toggleTodoCompleted}} />
+        {/each}
+      </ul>
+    {:else if !filteredTodos.length && filterTerm}
+      <div class="empty-todos-list">No todos found.</div>
+    {:else}
+      <div class="empty-todos-list">Add some stuff to get done!</div>
     {/if}
   </div>
-  <ul class="todos-list" id="todos-list">
-    {#if todos.length}
-      {#each todos as todo, i}
-        <Todo props={{todo, i, deleteTodo, toggleTodoCompleted}} />
-      {/each}
-    {:else}
-      <p>Add some stuff to get done!</p>
-    {/if}
-  </ul>
   <div class="add-todo-group">
     <form on:submit|preventDefault={addNewTodo}>
-      <input type="text" bind:value={newTodo} class="add-todo-title" placeholder="Enter new todo here">
+      <input type="text" bind:value={newTodo} class="add-todo-title" placeholder="Enter new todo here...">
       <button type="submit" class="add-todo btn">+ Add Todo</button>
     </form>
   </div>
